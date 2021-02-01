53ff04bd33dbb0a962f64d2c8168df610223970d
Carlos Martinez <curlosmart@gmail.com>
2021-02-01 09:21:05 -0500

    Post request can be sent

---
+++ Models/Issue.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const issueSchema = new Schema({
+    issue_title: {
+        type: String,
+        required: true
+    },
+    issue_text: {
+        type: String,
+        required: true
+    },
+    created_by: {
+        type: String,
+        required: true
+    },
+    assigned_to: String,
+    status_text: String,
+    created_on: Date,
+    updated_on: Date,
+    open: Boolean
+})
+
+module.exports = mongoose.model('Issue', issueSchema);
--- routes/api.js
+++ routes/api.js
@@ -1,25 +1,63 @@
 'use strict';
 
+const mongoose = require("mongoose");
+const url = process.env.DB;
+const Issue = require('../Models/Issue')
+
+mongoose
+	.connect(url, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+		useCreateIndex: true,
+	})
+	.then(() => {
+		console.log("connected to MongoDB");
+	})
+	.catch((error) => {
+		console.log("error connecting to MongoDB:", error.message);
+    });
+
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
   
-    .get(function (req, res){
+    .get(async (req, res) => {
       let project = req.params.project;
+      const allIssues = await Issue.find({})
+      res.json(allIssues);
       
     })
     
-    .post(function (req, res){
+    .post(async (req, res) => {
       let project = req.params.project;
+      let requiredFields = Object.keys(req.body).includes('issue_title') && Object.keys(req.body).includes('issue_text') && Object.keys(req.body).includes('created_by')
+      
+      if(requiredFields) {
+        let issue = new Issue({
+          issue_title: req.body.issue_title,
+          issue_text: req.body.issue_text,
+          created_by: req.body.created_by,
+          assigned_to: req.body.assigned_to,
+          status_text: req.body.status_text,
+          created_on: new Date(),
+          updated_on: new Date(),
+          open: true
+        })
+
+        issue.save((err, data) => {
+          res.json(data);
+        })
+      }
       
     })
     
-    .put(function (req, res){
+    .put(async (req, res) => {
       let project = req.params.project;
-      
+      console.log(req.body);
     })
     
-    .delete(function (req, res){
+    .delete(async (req, res) => {
       let project = req.params.project;
       
     });
